ad5d1d9c5ef6bc3ff459b272efc4b285e2420c5b
pari35 <paritoshpardeshi35@gmail.com>
2024-09-26 15:42:30 +0530

    config changes

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app = express()
 import cookieParser from "cookie-parser"
 import errorMiddleWare from "./middleware/error.js"
 import bodyParser from "body-parser"
-import fileUpload from "express-fileupload"
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(fileUpload())
 app.get('/about', (req, resp) => {
   resp.send("welcome to home page")
 })
